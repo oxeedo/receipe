1776cc4cad0f7ad946d1050702a4a62f6d8a5dd9
Gabriel <gabriel>
2022-05-29 18:39:24 +0100

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:recipe_app/firebase_options.dart';
+import 'package:recipe_app/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
